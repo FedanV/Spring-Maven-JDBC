2d09e591944e33ee0a7f32885948892b6b4993e7
VitaliiFedan <vitalii@herdwatch.com>
2022-08-02 14:26:04 +0300

    created methods createHeader, createBody, createReminder

--- springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/formatters/ClassicFormatter.java
+++ springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/formatters/ClassicFormatter.java
@@ -1,40 +1,84 @@
 package com.foxminded.vitaliifedan.task4.formatters;
 
-import com.foxminded.vitaliifedan.task4.models.Division;
+import com.foxminded.vitaliifedan.task4.models.Calculator;
 import com.foxminded.vitaliifedan.task4.models.Result;
+import com.foxminded.vitaliifedan.task4.models.Step;
+
+import java.util.List;
 
 public class ClassicFormatter implements Formatter {
 
     @Override
     public String format(Result result) {
 
+        StringBuilder classicResult = new StringBuilder();
+
         if (result.getDividend() < result.getDivisor()) {
-            return "" + result.getDividend() + " / " + result.getDivisor() + " = " + result.getQuotient();
+            String zeroQuotient = String.format("%d / %d = %d", result.getDividend(), result.getDivisor(), result.getQuotient());
+            return classicResult.append(zeroQuotient).toString();
         }
 
-        Division division = new Division();
-        StringBuilder stringResult = division.makeDivision(result.getDividend(), result.getDivisor());
+        classicResult.append(createHeader(result));
+        classicResult.append(createBody(result));
+        classicResult.append(createReminder(result));
 
-        int[] index = new int[3];
-        int j = 0;
-        for (int i = 0; i < stringResult.length(); i++) {
-            if (stringResult.charAt(i) == '\n') {
-                index[j] = i;
-                j++;
-            }
-            if (j == 3) {
-                break;
-            }
+        return classicResult.toString();
+    }
+
+    private String createReminder(Result result) {
+
+        int reminderIndent = Calculator.length(result.getDividend()) - Calculator.length(result.getReminder());
+        return assembleString(reminderIndent + 1, ' ') + result.getReminder();
+
+    }
+
+    private String createHeader(Result result) {
+
+        List<Step> steps = result.getSteps();
+        int dividendLength = Calculator.length(result.getDividend());
+        int subtrahendLength = Calculator.length(steps.get(0).getSubtrahend());
+        int minuendLength = Calculator.length(steps.get(0).getMinuend());
+        int quotientLength = Calculator.length(result.getQuotient());
+        int indent = Calculator.length(minuendLength + 1 - subtrahendLength);
+
+        StringBuilder headerString = new StringBuilder();
+
+        String firstRow = String.format("_%d│%d", result.getDividend(), result.getDivisor());
+        headerString.append(firstRow).append("\n");
+
+        String secondRow = assembleString(indent, ' ') +
+                steps.get(0).getSubtrahend() +
+                assembleString(dividendLength - subtrahendLength, ' ') +
+                "│" + assembleString(quotientLength, '-');
+        headerString.append(secondRow).append("\n");
+
+        String thirdRow = assembleString(indent, ' ') +
+                assembleString(subtrahendLength, '-') +
+                assembleString(dividendLength - subtrahendLength, ' ') +
+                "│" + result.getQuotient();
+        headerString.append(thirdRow).append("\n");
+
+        return headerString.toString();
+    }
+
+    private String createBody(Result result) {
+
+        StringBuilder bodyString = new StringBuilder();
+        List<Step> steps = result.getSteps();
+
+        for (int i = 1; i < steps.size(); i++) {
+
+            int subtrahendLength = Calculator.length(steps.get(i).getSubtrahend());
+            String minuend = assembleString(i - 1, ' ') + "_" + steps.get(i).getMinuend();
+            bodyString.append(minuend).append("\n");
+            String subtrahend = assembleString(i, ' ') + steps.get(i).getSubtrahend();
+            bodyString.append(subtrahend).append("\n");
+            String end = assembleString(i, ' ') + assembleString(subtrahendLength, '-');
+            bodyString.append(end).append("\n");
+
         }
-        int countOfIndents = division.calculateDigits(result.getDividend()) + 1 - index[0];
-        stringResult.insert(index[2], division.assebleString(countOfIndents, ' ') + "│" + result.getQuotient());
-        stringResult.insert(index[1], division.assebleString(countOfIndents, ' ') + "│"
-                + division.assebleString(String.valueOf(result.getQuotient()).length(), '-'));
-        stringResult.insert(index[0], "│" + result.getDivisor());
-        stringResult.replace(1, index[0], String.valueOf(result.getDividend()));
 
-
-        return stringResult.toString();
+        return bodyString.toString();
     }
 
 }
--- springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/formatters/GermanFormatter.java
+++ springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/formatters/GermanFormatter.java
@@ -1,37 +1,76 @@
 package com.foxminded.vitaliifedan.task4.formatters;
 
-import com.foxminded.vitaliifedan.task4.models.Division;
+import com.foxminded.vitaliifedan.task4.models.Calculator;
 import com.foxminded.vitaliifedan.task4.models.Result;
+import com.foxminded.vitaliifedan.task4.models.Step;
+
+import java.util.List;
 
 public class GermanFormatter implements Formatter {
 
     @Override
     public String format(Result result) {
 
+        StringBuilder germanResult = new StringBuilder();
+
         if (result.getDividend() < result.getDivisor()) {
-            return "" + result.getDividend() + " / " + result.getDivisor() + " = " + result.getQuotient();
+            String zeroQuotient = String.format("%d / %d = %d", result.getDividend(), result.getDivisor(), result.getQuotient());
+            return germanResult.append(zeroQuotient).toString();
         }
 
-        Division division = new Division();
-        StringBuilder stringResult = division.makeDivision(result.getDividend(), result.getDivisor());
+        germanResult.append(createHeader(result));
+        germanResult.append(createBody(result));
+        germanResult.append(createReminder(result));
 
-        int[] index = new int[1];
-        int j = 0;
-        for (int i = 0; i < stringResult.length(); i++) {
-            if (stringResult.charAt(i) == '\n') {
-                index[j] = i;
-                j++;
-            }
-            if (j == 1) {
-                break;
-            }
+        return germanResult.toString();
+    }
+
+    private String createReminder(Result result) {
+
+        int reminderIndent = Calculator.length(result.getDividend()) - Calculator.length(result.getReminder());
+        return assembleString(reminderIndent + 1, ' ') + result.getReminder();
+
+    }
+
+    private String createHeader(Result result) {
+
+        List<Step> steps = result.getSteps();
+        int subtrahendLength = Calculator.length(steps.get(0).getSubtrahend());
+        int minuendLength = Calculator.length(steps.get(0).getMinuend());
+        int indent = Calculator.length(minuendLength + 1 - subtrahendLength);
+
+        StringBuilder headerString = new StringBuilder();
+
+        String firstRow = String.format("_%d ÷ %d => %d", result.getDividend(), result.getDivisor(), result.getQuotient());
+        headerString.append(firstRow).append("\n");
+
+        String secondRow = assembleString(indent, ' ') + steps.get(0).getSubtrahend();
+        headerString.append(secondRow).append("\n");
+
+        String thirdRow = assembleString(indent, ' ') + assembleString(subtrahendLength, '-');
+        headerString.append(thirdRow).append("\n");
+
+        return headerString.toString();
+    }
+
+    private String createBody(Result result) {
+
+        StringBuilder bodyString = new StringBuilder();
+        List<Step> steps = result.getSteps();
+
+        for (int i = 1; i < steps.size(); i++) {
+
+            int subtrahendLength = Calculator.length(steps.get(i).getSubtrahend());
+            String minuend = assembleString(i - 1, ' ') + "_" + steps.get(i).getMinuend();
+            bodyString.append(minuend).append("\n");
+            String subtrahend = assembleString(i, ' ') + steps.get(i).getSubtrahend();
+            bodyString.append(subtrahend).append("\n");
+            String end = assembleString(i, ' ') + assembleString(subtrahendLength, '-');
+            bodyString.append(end).append("\n");
+
         }
 
-        stringResult.insert(index[0], " ÷ " + result.getDivisor() + " => " + result.getQuotient());
-        stringResult.replace(1, index[0], String.valueOf(result.getDividend()));
-
-
-        return stringResult.toString();
+        return bodyString.toString();
     }
 }
 
