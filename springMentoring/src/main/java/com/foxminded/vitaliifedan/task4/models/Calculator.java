9ba6faba000f603e913b959ad1b49514ae5ee98d
VitaliiFedan <vitalii@herdwatch.com>
2022-08-02 14:23:45 +0300

    changed class and added new methods

--- springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/models/Calculator.java
+++ springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/models/Calculator.java
@@ -1,5 +1,8 @@
 package com.foxminded.vitaliifedan.task4.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Calculator {
 
     public Result divide(int dividend, int divisor) {
@@ -12,16 +15,44 @@ public class Calculator {
         divisor = Math.abs(divisor);
 
         if (dividend < divisor) {
-            return new Result(dividend, divisor, 0, 1);
+            return new Result(dividend, divisor, 0, 1, null);
         }
 
-        Division division = new Division();
-        division.makeDivision(dividend, divisor);
+        List<Step> steps = new ArrayList<>();
+        int[] digits = numberToDigits(dividend);
+        int minuend = 0;
+        int quotient = 0;
 
-        int reminder = Integer.parseInt(division.getReminder().toString());
-        int quotient = Integer.parseInt(division.getQuotient().toString());
+        for (int digit : digits) {
+            minuend = minuend * 10 + digit;
+            int stepQuotient = minuend / divisor;
+            if (minuend >= divisor) {
+                int subtrahend = stepQuotient * divisor;
+                int difference = minuend - subtrahend;
+                quotient = quotient * 10 + stepQuotient;
+                steps.add(new Step(minuend, subtrahend, difference, stepQuotient));
+                minuend = difference;
+            } else {
+                quotient = quotient * 10 + stepQuotient;
+            }
+        }
 
-        return new Result(dividend, divisor, quotient, reminder);
+        int reminder = minuend;
+        return new Result(dividend, divisor, quotient, reminder, steps);
 
+    }
+
+    public static int length(int number) {
+        return number < 10 ? 1 : (int) Math.log10(number) + 1;
+    }
+
+    private static int[] numberToDigits(int number) {
+        int lengthNumber = length(number);
+        int[] result = new int[lengthNumber];
+        for (int i = lengthNumber - 1; i >= 0; i--) {
+            result[i] = number % 10;
+            number /= 10;
+        }
+        return result;
     }
 }
