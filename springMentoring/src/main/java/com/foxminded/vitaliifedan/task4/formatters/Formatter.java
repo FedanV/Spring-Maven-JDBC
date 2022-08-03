5f344a6dea88890f4709ace8e0d4ac3139291c64
VitaliiFedan <vitalii@herdwatch.com>
2022-08-03 08:26:16 +0300

    moved method assembleString to StringUtils

--- springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/formatters/Formatter.java
+++ springMentoring/src/main/java/com/foxminded/vitaliifedan/task4/formatters/Formatter.java
@@ -5,11 +5,4 @@ import com.foxminded.vitaliifedan.task4.models.Result;
 public interface Formatter {
     String format(Result result);
 
-    default String assembleString(int countSymbols, char symbol) {
-        StringBuilder string = new StringBuilder();
-        for (int i = 0; i < countSymbols; i++) {
-            string.append(symbol);
-        }
-        return string.toString();
-    }
 }
