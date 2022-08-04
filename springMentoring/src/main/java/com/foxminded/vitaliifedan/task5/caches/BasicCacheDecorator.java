1b7621fbe4b818f3dac356aa4fbf62e8148cc9d6
VitaliiFedan <vitalii@herdwatch.com>
2022-08-04 23:39:52 +0300

    it was created class with realise basic cache

---
+++ springMentoring/src/main/java/com/foxminded/vitaliifedan/task5/caches/BasicCacheDecorator.java
@@ -0,0 +1,42 @@
+package com.foxminded.vitaliifedan.task5.caches;
+
+import com.foxminded.vitaliifedan.task5.CharCounter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BasicCacheDecorator implements CharCounter, Cache<String, Map<Character, Integer>> {
+
+    private final Map<String, Map<Character, Integer>> cache;
+    private final CharCounter charCounter;
+
+    public BasicCacheDecorator(CharCounter charCounter) {
+        this.charCounter = charCounter;
+        this.cache = new HashMap<>();
+    }
+
+    @Override
+    public Map<Character, Integer> get(String key) {
+        return cache.get(key);
+    }
+
+    @Override
+    public void put(String key, Map<Character, Integer> value) {
+        cache.put(key, value);
+    }
+
+    @Override
+    public boolean contains(String key) {
+        return cache.containsKey(key);
+    }
+
+    @Override
+    public Map<Character, Integer> count(String text) {
+        String key = text.trim();
+        if (!contains(key)) {
+            put(key, charCounter.count(key));
+        }
+        return get(key);
+    }
+
+}
