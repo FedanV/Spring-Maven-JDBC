f7a1089bae2deb2b69b8adb089259444568af6e6
VitaliiFedan <vitalii@herdwatch.com>
2022-08-15 18:35:42 +0300

    created models

---
+++ springMentoring/src/main/java/com/foxminded/vitaliifedan/task7/models/Group.java
@@ -0,0 +1,46 @@
+package com.foxminded.vitaliifedan.task7.models;
+
+import java.util.Objects;
+
+public class Group {
+    private int groupId;
+    private String groupName;
+
+    public Group(int groupId, String groupName) {
+        this.groupId = groupId;
+        this.groupName = groupName;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public void setGroupName(String groupName) {
+        this.groupName = groupName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return groupId == group.groupId && Objects.equals(groupName, group.groupName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(groupId, groupName);
+    }
+
+    @Override
+    public String toString() {
+        return "Group{" +
+                "groupId=" + groupId +
+                ", groupName='" + groupName + '\'' +
+                '}';
+    }
+}
