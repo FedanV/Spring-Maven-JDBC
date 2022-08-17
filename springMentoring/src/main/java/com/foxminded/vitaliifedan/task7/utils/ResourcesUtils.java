7a389c673e09b5db96adefdafacda7a012dab3de
VitaliiFedan <vitalii@herdwatch.com>
2022-08-17 12:50:39 +0300

    add loadPropertiesFromResources

--- springMentoring/src/main/java/com/foxminded/vitaliifedan/task7/utils/ResourcesUtils.java
+++ springMentoring/src/main/java/com/foxminded/vitaliifedan/task7/utils/ResourcesUtils.java
@@ -1,16 +1,29 @@
 package com.foxminded.vitaliifedan.task7.utils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ResourcesUtils {
 
     private ResourcesUtils() {
+    }
+
+    public static Properties loadPropertiesFromResources(String fileName) throws IOException {
+        try(InputStream resourceAsStream = ResourcesUtils.class.getClassLoader().getResourceAsStream(fileName)) {
+            if(resourceAsStream == null) {
+                throw new IOException("File " + fileName + " not found.");
+            }
+            Properties properties = new Properties();
+            properties.load(resourceAsStream);
+            return properties;
+        }
     }
 
     public static String loadTextFileFromResources(String fileName) throws IOException {
